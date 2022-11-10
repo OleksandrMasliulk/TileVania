4d20a91594bb9dfa9358f232fa47a2e6542926d9
odrmasliuk <odr.masliuk@gmail.com>
2022-11-10 16:50:28 +0100

    Lecture 11 | Done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,12 +18,23 @@ public class PlayerMovement : MonoBehaviour
     private void Update() 
     {
         Move();    
+        FlipSprite();
     }
-    
+
     private void Move()
     {
         Vector2 newVelocity = new Vector2(_moveInput.x * _moveSpeed, _rigidbody2D.velocity.y);
         _rigidbody2D.velocity = newVelocity;
+    }
+
+    private void FlipSprite() 
+    {
+        bool isMovingHorizontally = Mathf.Abs(_rigidbody2D.velocity.x) > Mathf.Epsilon;
+
+        if (isMovingHorizontally)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(_rigidbody2D.velocity.x), 1f);
+        }
     }
 
     private void OnMove(InputValue value) => _moveInput = value.Get<Vector2>();
