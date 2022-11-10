46bf6e10bb7c78e4b6b5cc0ed0a849873529b6a5
odrmasliuk <odr.masliuk@gmail.com>
2022-11-10 16:35:22 +0100

    Lecture 10 | Done

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] private float _moveSpeed;
+
+    private Rigidbody2D _rigidbody2D;
+
     private Vector2 _moveInput;
 
-    private void OnMove(InputValue value)
+    private void Awake() 
     {
-        _moveInput = value.Get<Vector2>();
-        Debug.Log(_moveInput);
+        _rigidbody2D = GetComponent<Rigidbody2D>();
     }
+
+    private void Update() 
+    {
+        Move();    
+    }
+    
+    private void Move()
+    {
+        Vector2 newVelocity = new Vector2(_moveInput.x * _moveSpeed, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = newVelocity;
+    }
+
+    private void OnMove(InputValue value) => _moveInput = value.Get<Vector2>();
+
 }
